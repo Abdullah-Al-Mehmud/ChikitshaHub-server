5a7f56d960b9d36f808f154f4d5a6589d106fa63
MoRsHeD <mdmorshed0187@gmail.com>
2024-01-26 01:42:09 +0600

    created bmi apis

---
+++ Schema/BMISchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const bmiSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    height: {
+        type: Number,
+        required: true,
+    },
+    weight: {
+        type: Number,
+        required: true,
+    },
+    bmiResult: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+
+});
+
+module.exports = bmiSchema;
