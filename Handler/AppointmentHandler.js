aa8f60de503bcfeee45d4d0146b4bb91f46ad371
TahidulAlam <tahid.cse@gmail.com>
2024-02-16 01:34:16 +0600

    update

--- Handler/AppointmentHandler.js
+++ Handler/AppointmentHandler.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const AppointmentSchema = require("../Schema/AppointmentSchema");
 const { readMiddlewareMail } = require("../middleware/crudMiddleware");
+const { verifyToken } = require("../middleware/VerifyToken");
+const { verifyAdmin } = require("../middleware/VerifyAdmin");
 const Appointment = new mongoose.model("Appointment", AppointmentSchema);
 
-router.get("/", async (req, res) => {
+router.get("/", verifyToken, verifyAdmin, async (req, res) => {
   try {
-    const { email } = req.query;
-    const result = await Appointment.find({ email });
+    const result = await Appointment.find();
     res.send(result);
   } catch (error) {
     // console.log(error);
@@ -16,13 +17,34 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/doctors/:doctorEmail", verifyToken, async (req, res) => {
+  try {
+    const { doctorEmail } = req.params;
+    // console.log(doctorEmail);
+    const result = await Appointment.find({ doctorEmail });
+    res.send(result);
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ msg: "unable to find appointment data" });
+  }
+});
 
-
+router.get("/patients/:patientEmail", verifyToken, async (req, res) => {
+  try {
+    const { patientEmail } = req.params;
+    const result = await Appointment.find({ patientEmail });
+    res.send(result);
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ msg: "unable to find appointment data" });
+  }
+});
 
 // post the appointment data
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   try {
     const newAppointment = new Appointment(req.body);
+    console.log(newAppointment);
     await newAppointment.save();
     console.log(newAppointment);
     res.status(201).send({ message: "added successfully ", success: true });
@@ -31,6 +53,5 @@ router.post("/", async (req, res) => {
     res.status(500).json({ msg: "unable to save appointment data" });
   }
 });
-router.get("/:email", readMiddlewareMail);
 
 module.exports = router;
