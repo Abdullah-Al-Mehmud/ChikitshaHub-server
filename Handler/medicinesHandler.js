0f42d80614600973db7f9d807f1f992dacc0fe58
morsalinamunmun <morsalinamunmun@gmail.com>
2024-03-01 02:40:04 +0600

    meeting route change

--- Handler/medicinesHandler.js
+++ Handler/medicinesHandler.js
@@ -83,7 +83,7 @@ router.get("/patient/:email", async (req, res) => {
   }
 }); */
 
-router.get("/1/:meetingId", async (req, res) => {
+router.get("/meeting/:meetingId", async (req, res) => {
   try {
     const meetingId = req.params.meetingId;
     console.log(meetingId);
